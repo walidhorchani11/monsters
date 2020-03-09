6db8f00ab1e16b0cda5ae212c76168cd63878ea6
walid horchani <walid.horchani@oyez.fr>
2020-03-09 16:32:06 +0100

    component did mount event -fetch-setState

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters : [
-        {name: 'dracula',id: Math.random()},
-        {name: 'joker',id: Math.random()},
-        {name: 'zombie',id: Math.random()}
-      ]
+      monsters : []
     }
+  }
+
+  componentDidMount(){
+    //setting state here will trigger re-rendering
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then( response => {
+        console.log('in component did mount set state to reRender ');
+        return response.json();
+    })
+      .then(users => {this.setState({monsters : users})})
+      
   }
 
   render(){
     return (
       <div>
+      {console.log('component rendered ...')}
         {
           this.state.monsters.map( monster => (<h1 key={monster.id}>{monster.name}</h1>))
         }
