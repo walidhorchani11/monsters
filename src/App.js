8508454713b338eebf76895a636d11081399ebf7
walid horchani <walid.horchani@oyez.fr>
2020-03-10 14:38:29 +0100

    cardlist and card components + searchFeild in state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      monsters : []
+      monsters : [],
+      searchField : ''
     }
   }
 
@@ -21,13 +22,24 @@ class App extends Component {
       
   }
 
+  handleChange = (event) => {
+    let searchInput = event.target.value;
+    this.setState({searchField: searchInput})
+    //filter monsters selon searchField
+
+  }
+
   render(){
     return (
       <div>
-      {console.log('component rendered ...')}
-        {
-          this.state.monsters.map( monster => (<h1 key={monster.id}>{monster.name}</h1>))
-        }
+        {console.log('component rendered ...')}
+        <div>
+          <input type='search' placeholder='search monster' onChange={e => {
+            this.setState({searchField: e.target.value}, () => console.log(this.state.searchField));
+            
+            }}/>
+        </div>
+        <CardList monsters = {this.state.monsters}/>
       </div>
     )
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card-list.styles.css';
+import {Card} from '../card/card.component';
+
+export const CardList = (props) => 
+  (
+    <div className='card-list'>
+      {console.log('card list rendered')}
+      {props.monsters.map(monster => <Card monster={monster} key={monster.id} />)} 
+    </div>
+  )
