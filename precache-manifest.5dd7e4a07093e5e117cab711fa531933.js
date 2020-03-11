60052890b14abfafcb2fdef49dca6a12e0b568a3
walid horchani <walid.horchani@oyez.fr>
2020-03-11 20:07:00 +0100

    Updates

---
+++ precache-manifest.5dd7e4a07093e5e117cab711fa531933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eec0b841b7b136c76f12e990e257589",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "cd2660cc998e3d3d36df",
+    "url": "/monsters/static/css/main.26e1b188.chunk.css"
+  },
+  {
+    "revision": "112b3bec2e48f2c07994",
+    "url": "/monsters/static/js/2.0091aeb8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.0091aeb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2660cc998e3d3d36df",
+    "url": "/monsters/static/js/main.957d1035.chunk.js"
+  },
+  {
+    "revision": "284a663af39cf5860fa6",
+    "url": "/monsters/static/js/runtime-main.2e4a3575.js"
+  }
+]);
