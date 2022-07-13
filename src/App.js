a68bf161eeccedf4aa6fb5c030f2084e0492bc38
walid horchani <walid.horchani@oyez.fr>
2022-07-13 17:43:47 +0100

    fix22

--- src/App.js
+++ src/App.js
@@ -48,14 +48,20 @@ const App = () => {
       {status === STATUS_REQUEST.SUCCESS && (
         <div>
           <h1>Monsters Rolodex</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+        </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
         </a>
+          </header>
           <SearchBox placeholder="search monster" handleSearch={handleSearch} />
           <CardList
             monsters={monsters.filter((elem) => {
